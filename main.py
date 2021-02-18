4f887254d67576a187fec4c1fd3741ca519aa533
tnc11 <tnc1@sfu.ca>
2021-02-18 06:34:34 +0000

    created first command

---
+++ main.py
@@ -0,0 +1,21 @@
+# https://www.freecodecamp.org/news/create-a-discord-bot-with-python/
+
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    # do not read messages from itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
