39fb76e8ab76ee6af6e01ec1e397d200800803bc
tnc11 <tnc1@sfu.ca>
2021-02-19 03:58:51 +0000

    added stock price lookup functionality

--- main.py
+++ main.py
@@ -6,37 +6,30 @@ from stockInfo import *
 
 client = discord.Client()
 
-url = 'https://finance.yahoo.com/gainers/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-mostGain = soup.findAll('tr')[1]
-
-for child in mostGain.children:
-	print(child)
-	print(" ")
-
-
 @client.event
 async def on_ready():
-	print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
 
 
 @client.event
 async def on_message(message):
-	# do not read messages from itself
-	if message.author == client.user:
-		return
+    # do not read messages from itself
+    if message.author == client.user or not message.content.startswith('$$'):
+        return
 
-	# gives the command to play David's playlist
-	if message.content.startswith('$David'):
-		await message.channel.send(
-		    '-p https://open.spotify.com/playlist/2pOCpGDfekUKDN6Mzr1NGi')
+    # gives the command to play David's playlist
+    if message.content.startswith('$$ David'):
+        await message.channel.send(
+            '-p https://open.spotify.com/playlist/2pOCpGDfekUKDN6Mzr1NGi')
 
-	if message.content.startswith('$mostgain'):
-		await message.channel.send(mostGain())
+    if message.content.startswith('$$ mostgain'):
+        await message.channel.send(mostGain())
 
-	if message.content.startswith('$mostloss'):
-		await message.channel.send(mostLoss())
+    if message.content.startswith('$$ mostloss'):
+        await message.channel.send(mostLoss())
+
+    if message.content.startswith('$$ price '):
+        await message.channel.send(currentPrice(message.content[9:]))
 
 
 client.run(os.getenv('TOKEN'))
--- stockInfo.py
+++ stockInfo.py
@@ -2,21 +2,33 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+import yfinance as yf
+
 
 def mostGain():
-  url = 'https://finance.yahoo.com/gainers/'
-  response = requests.get(url)
-  soup = BeautifulSoup(response.text, "html.parser")
-  mostGain = soup.findAll('tr')[1]
-  
-  name = mostGain.a
-  return name.string
+    url = 'https://finance.yahoo.com/gainers/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    mostGain = soup.findAll('tr')[1]
+
+    name = mostGain.a
+    return name.string
 
 def mostLoss():
-  url = 'https://finance.yahoo.com/losers/'
-  response = requests.get(url)
-  soup = BeautifulSoup(response.text, "html.parser")
-  mostLoss = soup.findAll('tr')[1]
+    url = 'https://finance.yahoo.com/losers/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    mostLoss = soup.findAll('tr')[1]
 
-  name = mostLoss.a
-  return name.string
+    name = mostLoss.a
+    return name.string
+
+def currentPrice(symbol):
+    if symbol == "DAVID":
+        return "FREE"
+    try:
+        ticker = yf.Ticker(symbol)
+        data = ticker.history(period='1d')
+        return data['Close'][0]
+    except:
+        return "could not find symbol"
